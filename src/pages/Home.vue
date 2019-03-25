2f90df9a0fe0e4d983a7dcabea35d240ef0c1406
Jakob Jensen <jvj@eucsyd.dk>
2019-03-25 12:32:23 +0100

    Initial commit

---
+++ src/pages/Home.vue
@@ -0,0 +1,20 @@
+<!--     ./src/pages/Home.vue      -->
+
+<template>
+    <div>
+        <div class="title">
+            <h1>{{msg}}</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'home',
+    data () {
+        return {
+            msg: 'Velkommen til min webshop'
+        }
+    }
+}
+</script>
