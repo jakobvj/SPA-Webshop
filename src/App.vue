67944ea1910169c880d3a67b51afbe2eb036420b
Jakob Jensen <jvj@eucsyd.dk>
2019-03-25 12:32:23 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<!-- ./src/App.vue -->
+<template>
+  <div>
+    <nav>
+      <div class="container">
+        <ul class="nav__left">
+
+          <!-- Navigation Links -->
+          <li><router-link to="/">Home</router-link></li>
+          <li><router-link to="/admin">Admin</router-link></li>
+          <li><router-link to="/cart">Cart</router-link></li>
+
+        </ul>
+      </div>
+    </nav>
+
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  //...
+};
+</script>
